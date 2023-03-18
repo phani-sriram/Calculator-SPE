54feedf6a252f6321fce71ca120a7102f06d6077
Blackthorn <phanisriram02@gmail.com>
2023-03-18 11:27:49 +0530

    Webhooks Demo

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.InputMismatchException;
-// Test Webhooks
+
 public class Calculator {
     private static final Logger logger = LogManager.getLogger(Calculator.class);
     public static void main(String[] args) {
